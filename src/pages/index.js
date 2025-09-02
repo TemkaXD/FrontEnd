f02526453f81a7e28fef994baa71a3268b8c389b
Your Name <you@example.com>
2025-09-02 13:05:53 +0800

    frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,115 +1,111 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-gradient-to-r from-pink-300 via-indigo-600 to-purple-600 text-white">
+      <div className="max-w-screen-lg mx-auto p-10">
+        <div className="flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0 mb-12">
+          <div className="flex flex-col items-center text-center md:text-left space-y-4">
+            
+            <div className="w-32 h-32  border-4 border-white object-cover shadow-xl">
+              <Image 
+                src="/zurag.png"
+                alt="Temuujin Enkhdul"
+                width={128}
+                height={128}
+              />
+            </div>
+            <h1 className="text-5xl font-extrabold leading-tight">Temuujin Enkhdul</h1>
+            
+            <div className="space-y-2 text-lg text-indigo-100">
+              <p>Email: <span className="font-medium">hlotmka@email.com</span></p>
+              <p>LinkedIn: <span className="font-medium">linkedin.com/in/temka</span></p>
+            </div>
+            <a href="#contact" className="mt-6 px-8 py-3 bg-white text-indigo-900 rounded-full text-lg font-semibold shadow-lg hover:bg-gray-100 transition duration-300 ease-in-out">
+              Contact Me
+            </a>
+          </div>
+
+          <div className="text-center md:text-left space-y-4 max-w-md mx-auto md:max-w-full">
+            <h2 className="text-3xl font-semibold text-indigo-100 pl-64">About Me</h2>
+            <p className="text-lg text-indigo-200">
+              Anh Nestd orj ireel IT giin talaar surj ehelsn. Python Programming helnuudig ehnees ni 4 jil garui uzj bn. Buh l python turliin helnuudig anhnaas ni surj bui intermediate baagi bnaa ez
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h2 className="text-3xl font-semibold text-indigo-100">Experience</h2>
+            <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-8 hover:scale-105 transition duration-300 ease-in-out">
+              <div className="flex items-center space-x-4 mb-4">
+                <div className="text-indigo-500 text-4xl">
+                  <i className="fas fa-briefcase"></i>
+                </div>
+                <h3 className="text-xl font-semibold">Software Engineer</h3>
+              </div>
+              <p className="text-sm text-indigo-500">Turshlaguud</p>
+              <ul className="list-disc list-inside text-indigo-900 mt-2 space-y-2">
+                <li>2014-2015 Ulsiin surguuli</li>
+                <li>2015-2020 Tusgal school</li>
+                <li>2020-2024 Nest IT School</li>
+              </ul>
+            </div>
+
+            <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-8 hover:scale-105 transition duration-300 ease-in-out">
+              <div className="flex items-center space-x-4 mb-4">
+                <div className="text-indigo-500 text-4xl">
+                  <i className="fas fa-briefcase"></i>
+                </div>
+                <h3 className="text-xl font-semibold">Achievements</h3>
+              </div>
+              <p className="text-sm text-indigo-500">Amjiltuud</p>
+              <ul className="list-disc list-inside text-indigo-900 mt-2 space-y-2">
+                <li>Plat3 in Valorant</li>
+                <li>Tuuhiin olympiad top 10</li>
+                <li>Chess 1000 elo</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="space-y-8">
+            <h2 className="text-3xl font-semibold text-indigo-100">Skills</h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 pt-10">
+              <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-6 text-center hover:scale-105 transition duration-300 ease-in-out">
+                <p className="font-semibold text-lg">Java</p>
+                <p className="text-sm text-indigo-500">Advanced</p>
+              </div>
+              <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-6 text-center hover:scale-105 transition duration-300 ease-in-out">
+                <p className="font-semibold text-lg">C++</p>
+                <p className="text-sm text-indigo-500">Advanced</p>
+              </div>
+              <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-6 text-center hover:scale-105 transition duration-300 ease-in-out">
+                <p className="font-semibold text-lg">Javascript</p>
+                <p className="text-sm text-indigo-500">Intermediate</p>
+              </div>
+              <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-6 text-center hover:scale-105 transition duration-300 ease-in-out">
+                <p className="font-semibold text-lg">Python</p>
+                <p className="text-sm text-indigo-500">Intermediate</p>
+              </div>
+              <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-6 text-center hover:scale-105 transition duration-300 ease-in-out">
+                <p className="font-semibold text-lg">Git</p>
+                <p className="text-sm text-indigo-500">Advanced</p>
+              </div>
+              <div className="bg-white text-indigo-900 shadow-2xl rounded-xl p-6 text-center hover:scale-105 transition duration-300 ease-in-out">
+                
+                <p className="font-semibold text-lg">SQL</p>
+                <p className="text-sm text-indigo-500">Advanced</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="contact" className="text-center mt-16">
+          <h2 className="text-3xl font-semibold text-indigo-100">Let's Connect!</h2>
+          <p className="text-lg text-indigo-200 mt-2">Feel free to reach out for any inquiries or collaboration opportunities</p>
+          </div>
+      </div>
     </div>
   );
 }
